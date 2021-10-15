c5908fba09275985e1a74ed5a401cda47752e005
chynnalew <chynnalew@yahoo.com>
2021-10-15 09:23:43 -0700

    add index controllers, home, machines, engineers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
-using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index() 
     { 
-      return View(); 
+      dynamic model = new ExpandoObject();
+      model.Engineer = _db.Engineers.ToList();
+      model.Machine = _db.Machines.ToList();
+      return View(model); 
     }
   }
 }
